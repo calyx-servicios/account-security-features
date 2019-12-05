03784575f87cabbe09de8cdf8adfb44ed551effe
mariodmoreno <mariodmoreno@gmail.com>
2019-12-05 18:06:56 -0300

    [ADD] [partner_supplier_requireds]

---
+++ partner_supplier_requireds/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Partner Suṕplier Requireds",
+
+    'summary': """ """,
+    'description': """ """,
+    'author': "Calyx",
+    'website': "http://www.calyxservicios.com.ar",
+    'category': 'Accounting',
+    'version': '11.0.1.0.0',
+    'depends': ['base', 'contacts', 'l10n_ar_partner', 'l10n_ar_account'],
+    'data': [
+        'views/res_partner_view.xml',
+    ],
+}
